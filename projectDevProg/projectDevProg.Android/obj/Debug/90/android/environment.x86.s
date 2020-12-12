725b8ec8abe2326ac127063d28876465018d9156
Bent Roggeman <bent.roggeman@student.howest.be>
2020-12-12 15:30:14 +0100

    UI aanpassen

--- projectDevProg/projectDevProg.Android/obj/Debug/90/android/environment.x86.s
+++ projectDevProg/projectDevProg.Android/obj/Debug/90/android/environment.x86.s
@@ -71,7 +71,7 @@ mono_aot_mode_name:
 	.section	.rodata.env.str.8,"aMS",@progbits,1
 	.type	.L.env.str.8, @object
 .L.env.str.8:
-	.asciz	"0f2a5375-a917-443e-adbb-673306c5600a"
+	.asciz	"f6df5bc4-6875-4fa6-9c5a-af152dc60580"
 	.size	.L.env.str.8, 37
 	.section	.rodata.env.str.9,"aMS",@progbits,1
 	.type	.L.env.str.9, @object
